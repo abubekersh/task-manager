ad685b6a533d19383abd4cd213005c967fc39adc
abubeker <abakabrak@gmail.com>
2025-05-06 08:42:14 +0300

    created the model and controller, defined basic operation of the task management here,all crud operations

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->filter == "completed") {
+            $tasks = Task::where('is_completed', '=', '1')->orderBy('is_completed')->orderByDesc('created_at')->paginate(4);
+        } elseif ($request->filter == "pending") {
+            $tasks = Task::where('is_completed', '=', '0')->orderBy('is_completed')->orderByDesc('created_at')->paginate(4);
+        } else {
+            $tasks = Task::where('id', '>', '1')->orderBy('is_completed')->orderByDesc('created_at')->paginate(4);
+        }
+        return view('tasks.index', ['tasks' => $tasks, 'filter' => $request->filter]);
+    }
+    public function create()
+    {
+        return view('tasks.create');
+    }
+    public function store(Request $request)
+    {
+        $task = $request->validate([
+            'title' => 'required|string|max:255|min:5',
+            'description' => 'max:500'
+        ]);
+        Task::create($task);
+        return redirect('/');
+    }
+    public function edit(int $task)
+    {
+
+        return view('tasks.edit', ['task' => Task::find($task)]);
+    }
+    public function update(Request $request)
+    {
+        $task = $request->validate([
+            'title' => 'required|string|max:255|min:5',
+            'description' => 'max:500'
+        ]);
+        $t = Task::find($request->id);
+        $t->title = $task['title'];
+        $t->description = $task['description'];
+        $t->save();
+        return redirect('/');
+    }
+    public function destroy(int $task)
+    {
+        Task::destroy($task);
+        return redirect('/');
+    }
+    public function complete(int $task)
+    {
+        $task = Task::find($task);
+        $task->is_completed = true;
+        $task->save();
+        return redirect()->back();
+    }
+}
