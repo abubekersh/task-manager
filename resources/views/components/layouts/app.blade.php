a15b6ea771a2869bc684e6491ef732b3df3ccbc0
abubeker <abakabrak@gmail.com>
2025-05-06 08:41:02 +0300

    created the basic setup like layouts and database migrations.

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$title}}</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-black text-white">
+    {{$slot}}
+</body>
+
+</html>
