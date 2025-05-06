d442434d25c8ad378165b727be2b2a956cc9108b
abubeker <abakabrak@gmail.com>
2025-05-06 08:42:36 +0300

    correspomding views of each page

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,24 @@
+<x-layouts.app>
+    <x-slot:title>Edit Task {{$task->title}}</x-slot>
+        <h1 class="text-3xl mt-10 text-center">Edit Task {{$task->title}}</h1>
+
+        <div class="mt-5 mb-5 border-3 px-10 py-5 rounded w-1/2 m-auto border-white">
+            <form action="/tasks/update" method="post">
+                @csrf
+                <a href="/" class="font-bold p-2 ml-auto border border-white w-fit block rounded">Back</a>
+                <input type="hidden" value="{{$task->id}}" name="id">
+                <div class="mb-3"><label for="title">Title</label><br> <input class="mt-1 w-full border border-white rounded p-2" type="text" value="{{$task->title}}" name="title" id="title">
+                    @error('title')
+                    <p class="mt-1 text-xs text-red-500">{{$message}}</p>
+                    @enderror
+                </div>
+                <div><label for="description">Description</label><br><textarea rows="5" class="mt-1 w-full border border-white rounded p-2" name="description" id="description">{{$task->description}}</textarea>
+                    @error('description')
+                    <p class="text-xs text-red-500">{{$message}}</p>
+                    @enderror
+                </div>
+                <button class="p-2 bg-white text-black rounded w-full mt-5">Update</button>
+                <a href="/tasks/{{$task->id}}/delete" class="block text-center p-2 bg-red-500 text-white rounded w-full mt-5">Delete</a>
+            </form>
+        </div>
+</x-layouts.app>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,44 @@
+<x-layouts.app>
+    <x-slot:title>Task Manager</x-slot>
+        <h1 class="text-3xl mt-10 text-center">Task Manager</h1>
+        @php
+        $bg = [
+        'bg-gradient-to-t from-blue-500 to-green-500',
+        'bg-gradient-to-t from-red-500 to-yellow-500',
+        'bg-gradient-to-t from-purple-500 to-pink-500 ',
+        'bg-gradient-to-t from-teal-500 to-blue-500 ',
+        'bg-gradient-to-t from-indigo-500 to-blue-500 '
+        ];
+        @endphp
+        <div class="mt-5 mb-5 border-3 px-10 py-5 rounded w-1/2 m-auto border-white">
+            <form action="/" method="get">
+                <label for="filter">Filter</label> <select class="
+                border border-white bg-black p-1 rounded" name="filter" id="filter">
+                    <option value="all" {{$filter=="all"?'selected':''}}>All</option>
+                    <option value="pending" {{$filter=="pending"?'selected':''}}>Pending</option>
+                    <option value="completed" {{$filter=="completed"?'selected':''}}>Completed</option>
+                </select>
+                <button class="border border-white p-1 rounded text-black bg-white">apply</button>
+            </form>
+            @foreach ($tasks as $index => $task)
+            <div class="mt-4 rounded  p-4 h-24   {{$task->is_completed?'bg-gradient-to-t from-gray-500 to-black-500':$bg[$index % count($bg)]}} flex">
+                <div class="grow">
+                    <p class="text-xl text-center  font-bold  {{$task->is_completed?'line-through':''}}">{{$task->title}}</p>
+                    <p class="text-center p-2 text-xs {{$task->is_completed?'line-through':''}}">{{$task->description}}</p>
+                </div>
+                <div class="mt-auto flex gap-2">
+                    @if ($task->is_completed)
+                    <a href="/tasks/{{$task->id}}/delete" class=" bg-white p-1 text-xs rounded text-red-400">Remove</a>
+                    @else
+                    <a href="/tasks/{{$task->id}}/edit" class=" bg-white p-1 text-xs rounded text-blue-400">Edit</a>
+                    <a href="/tasks/{{$task->id}}/complete" class=" bg-white p-1 text-xs rounded text-blue-400">Done</a>
+                    @endif
+                </div>
+            </div>
+            @endforeach
+            <div class="text-center my-5 border p-1 border-white">
+                <a href="/tasks/create" class="flex flex-col font-bold"> <span>+</span>Add New Task</a>
+            </div>
+            {{$tasks->links()}}
+        </div>
+</x-layouts.app>
