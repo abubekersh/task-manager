8f31edf25e720d47827d9d5124e7a41fe3518115
abubeker <abakabrak@gmail.com>
2025-05-06 08:41:31 +0300

    defined basic roues of the project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index']);
+Route::get('/tasks/create', [TaskController::class, 'create']);
+Route::post('/tasks/store', [TaskController::class, 'store']);
+Route::get('/tasks/{id}/complete', [TaskController::class, 'complete']);
+Route::get('/tasks/{id}/delete', [TaskController::class, 'destroy']);
+Route::get('/tasks/{id}/edit', [TaskController::class, 'edit']);
+Route::post('/tasks/update', [TaskController::class, 'update']);
